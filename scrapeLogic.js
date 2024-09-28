84496e290e8c38721b1cc7fe44567bb8a1f47407
Om-Chavan501 <om.chavan501@gmail.com>
2024-09-28 15:56:05 +0530

    shell script added

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -6,16 +6,16 @@ const scrapeLogic = async (res) => {
 
   const browser = await puppeteer.launch({
     headless: true,
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
+    // args: [
+    //   "--disable-setuid-sandbox",
+    //   "--no-sandbox",
+    //   "--single-process",
+    //   "--no-zygote",
+    // ],
+    // executablePath:
+    //   process.env.NODE_ENV === "production"
+    //     ? process.env.PUPPETEER_EXECUTABLE_PATH
+    //     : puppeteer.executablePath(),
   });
   console.log("Browser launched successfully.");
 
